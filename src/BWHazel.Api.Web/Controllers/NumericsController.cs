5f1287e880533fe5cd367e8cc01634c840a6b3ca
Benedict Hazel <bwhazel@gmail.com>
2021-08-14 22:01:22 +0100

    Add 2x2 matrix operations to new algebraics controller and legacy operations to numerics controller.

--- src/BWHazel.Api.Web/Controllers/NumericsController.cs
+++ src/BWHazel.Api.Web/Controllers/NumericsController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using BWHazel.Api.Core.Algebraics;
 using BWHazel.Api.Core.Numerics;
+using BWHazel.Api.Web.Models;
 
 namespace BWHazel.Api.Web.Controllers
 {
@@ -13,6 +15,15 @@ namespace BWHazel.Api.Web.Controllers
     [Route("api/[controller]")]
     public class NumericsController : Controller
     {
+        /// <summary>
+        /// The algebraics controller.
+        /// </summary>
+        /// <remarks>
+        /// This is included to support legacy matrix operations which will be
+        /// retained until 1st January 2022.
+        /// </remarks>
+        private readonly AlgebraicsController algebraicsController = new();
+        
         /// <summary>
         /// Converts a decimal number into binary.
         /// </summary>
@@ -298,6 +309,177 @@ namespace BWHazel.Api.Web.Controllers
         public ActionResult<uint[]> GetUnhappyLoopNumbers()
         {
             return this.Ok(RecreationalMathematics.UnhappyLoopNumbers);
+        }
+
+        /// <summary>
+        /// Gets the determinant, trace and eigenvalues of a 2x2 real matrix.
+        /// </summary>
+        /// <param name="w">The element in row 1, column 1.</param>
+        /// <param name="x">The element in row 1, column 2.</param>
+        /// <param name="y">The element in row 2, column 1.</param>
+        /// <param name="z">The element in row 2, column 2.</param>
+        /// <remarks>
+        /// This method is now obsolete and shall be removed on 1st January 2022.
+        /// Please use "/api/algebraics/linear/matrix2/e11/{e11}/e12/{e12}/e21/{e21}/e22/{e22}" instead.
+        /// </remarks>
+        /// <returns>The determinant, trace and eigenvalues of a 2x2 real matrix.</returns>
+        /// <response code="200">Returns the determinant, trace and eigenvalues of a 2x2 real matrix.</response>
+        /// <response code="400">The eigenvalues calculation involves a negative discriminant.</response>
+        [Obsolete]
+        [HttpGet]
+        [Route("matrix2/w/{w}/x/{x}/y/{y}/z/{z}")]
+        public ActionResult<RealMatrix2Info> GetRealMatrix2InfoLegacy(double w, double x, double y, double z)
+        {
+            return this.algebraicsController.GetRealMatrix2Info(w, x, y, z);
+        }
+
+        /// <summary>
+        /// Gets the determinant of a 2x2 real matrix.
+        /// </summary>
+        /// <param name="w">The element in row 1, column 1.</param>
+        /// <param name="x">The element in row 1, column 2.</param>
+        /// <param name="y">The element in row 2, column 1.</param>
+        /// <param name="z">The element in row 2, column 2.</param>
+        /// <remarks>
+        /// This method is now obsolete and shall be removed on 1st January 2022.
+        /// Please use "/api/algebraics/linear/matrix2/e11/{e11}/e12/{e12}/e21/{e21}/e22/{e22}/determinant" instead.
+        /// </remarks>
+        /// <returns>The determinant of a 2x2 real matrix.</returns>
+        /// <response code="200">Returns the determinant of a 2x2 real matrix.</response>
+        [Obsolete]
+        [HttpGet]
+        [Route("matrix2/w/{w}/x/{x}/y/{y}/z/{z}/determinant")]
+        public ActionResult<double> GetRealMatrix2DeterminantLegacy(double w, double x, double y, double z)
+        {
+            return this.algebraicsController.GetRealMatrix2Determinant(w, x, y, z);
+        }
+
+        /// <summary>
+        /// Gets the trace of a 2x2 real matrix.
+        /// </summary>
+        /// <param name="w">The element in row 1, column 1.</param>
+        /// <param name="x">The element in row 1, column 2.</param>
+        /// <param name="y">The element in row 2, column 1.</param>
+        /// <param name="z">The element in row 2, column 2.</param>
+        /// <remarks>
+        /// This method is now obsolete and shall be removed on 1st January 2022.
+        /// Please use "/api/algebraics/linear/matrix2/e11/{e11}/e12/{e12}/e21/{e21}/e22/{e22}/trace" instead.
+        /// </remarks>
+        /// <returns>The trace of a 2x2 real matrix.</returns>
+        /// <response code="200">Returns the trace of a 2x2 real matrix.</response>
+        [Obsolete]
+        [HttpGet]
+        [Route("matrix2/w/{w}/x/{x}/y/{y}/z/{z}/trace")]
+        public ActionResult<double> GetRealMatrix2TraceLegacy(double w, double x, double y, double z)
+        {
+            return this.algebraicsController.GetRealMatrix2Trace(w, x, y, z);
+        }
+
+        /// <summary>
+        /// Gets the eigenvalues of a 2x2 real matrix.
+        /// </summary>
+        /// <param name="w">The element in row 1, column 1.</param>
+        /// <param name="x">The element in row 1, column 2.</param>
+        /// <param name="y">The element in row 2, column 1.</param>
+        /// <param name="z">The element in row 2, column 2.</param>
+        /// <remarks>
+        /// This method is now obsolete and shall be removed on 1st January 2022.
+        /// Please use "/api/algebraics/linear/matrix2/e11/{e11}/e12/{e12}/e21/{e21}/e22/{e22}/eigenvalues" instead.
+        /// </remarks>
+        /// <returns>The eigenvalues of a 2x2 real matrix.</returns>
+        /// <response code="200">Returns the eigenvalues of a 2x2 real matrix.</response>
+        /// <response code="400">The eigenvalues calculation involves a negative discriminant.</response>
+        [Obsolete]
+        [HttpGet]
+        [Route("matrix2/w/{w}/x/{x}/y/{y}/z/{z}/eigenvalues")]
+        public ActionResult<double[]> GetRealMatrix2EigenvaluesLegacy(double w, double x, double y, double z)
+        {
+            return this.algebraicsController.GetRealMatrix2Eigenvalues(w, x, y, z);
+        }
+
+        /// <summary>
+        /// Gets the principal eigenvalue index of a 2x2 real matrix from the eigenvalues
+        /// array returned from the Eigenvalues endpoint.
+        /// </summary>
+        /// <param name="w">The element in row 1, column 1.</param>
+        /// <param name="x">The element in row 1, column 2.</param>
+        /// <param name="y">The element in row 2, column 1.</param>
+        /// <param name="z">The element in row 2, column 2.</param>
+        /// <remarks>
+        /// This method is now obsolete and shall be removed on 1st January 2022.
+        /// Please use "/api/algebraics/linear/matrix2/e11/{e11}/e12/{e12}/e21/{e21}/e22/{e22}/eigenvalues/principal" instead.
+        /// </remarks>
+        /// <returns>The principal eigenvalue index of a 2x2 real matrix.</returns>
+        /// <response code="200">Returns the principal eigenvalue index of a 2x2 real matrix.</response>
+        /// <response code="400">The eigenvalues calculation involves a negative discriminant.</response>
+        [Obsolete]
+        [HttpGet]
+        [Route("matrix2/w/{w}/x/{x}/y/{y}/z/{z}/eigenvalues/principal")]
+        public ActionResult<int> GetRealMatrix2PrincipalEigenvalueIndexLegacy(double w, double x, double y, double z)
+        {
+            return this.algebraicsController.GetRealMatrix2PrincipalEigenvalueIndex(w, x, y, z);
+        }
+
+        /// <summary>
+        /// Calculates the value for an eigenvector of a 2x2 real matrix.
+        /// </summary>
+        /// <param name="w">The element in row 1, column 1.</param>
+        /// <param name="x">The element in row 1, column 2.</param>
+        /// <param name="y">The element in row 2, column 1.</param>
+        /// <param name="z">The element in row 2, column 2.</param>
+        /// <param name="eigenvalue">The eigenvalue.</param>
+        /// <param name="value">The substituted value.</param>
+        /// <remarks>
+        /// This method is now obsolete and shall be removed on 1st January 2022.
+        /// Please use "/api/algebraics/linear/matrix2/e11/{e11}/e12/{e12}/e21/{e21}/e22/{e22}/eigenvector/{eigenvalue}/{value}" instead.
+        /// </remarks>
+        /// <returns>The value for an eigenvector for a specified eigenvalue of a 2x2 real matrix.</returns>
+        /// <response code="200">Returns the eigenvector value for a specified eigenvelue of a 2x2 real matrix.</response>
+        [Obsolete]
+        [HttpGet]
+        [Route("matrix2/w/{w}/x/{x}/y/{y}/z/{z}/eigenvector/{eigenvalue}/{value}")]
+        public ActionResult<double> GetRealMatrix2EigenvectorLegacy(double w, double x, double y, double z, double eigenvalue, double value)
+        {
+            return this.algebraicsController.GetRealMatrix2Eigenvector(w, x, y, z, eigenvalue, value);
+        }
+
+        /// <summary>
+        /// Calculate a series of values for an eigenvector of a 2x2 real matrix.
+        /// </summary>
+        /// <param name="w">The element in row 1, column 1.</param>
+        /// <param name="x">The element in row 1, column 2.</param>
+        /// <param name="y">The element in row 2, column 1.</param>
+        /// <param name="z">The element in row 2, column 2.</param>
+        /// <param name="start">The substituted value at the start of the series.</param>
+        /// <param name="increment">The increment between substituted values.</param>
+        /// <param name="total">The total number of values in the series.</param>
+        /// <remarks>
+        /// This method is now obsolete and shall be removed on 1st January 2022.  Please note this endpoint is not being replaced.
+        /// Please use the "api/Algebraics/linear/matrix2/e11/{e11}/e12/{e12}/e21/{e21}/e22/{e22}/eigenvector/{eigenvalue}/{value}"
+        /// endpoint to calculate individual values.
+        /// </remarks>
+        /// <returns>The series of values for an eigenvector of a 2x2 real matrix.</returns>
+        /// <response code="200">Returns the eigenvector value series for a specified eigenvelue of a 2x2 real matrix.</response>
+        [Obsolete]
+        [HttpGet]
+        [Route("matrix2/w/{w}/x/{x}/y/{y}/z/{z}/eigenvectors/start/{start}/increment/{increment}/total/{total}")]
+        public ActionResult<double[][]> GetRealMatrix2EigenvectorsLegacy(double w, double x, double y, double z, double start, double increment, int total)
+        {
+            RealMatrix2 realMatrix2 = new(w, x, y, z);
+            double[] eigenvalues = realMatrix2.GetEigenvalues();
+
+            List<double[]> eigenvectors = new();
+            double currentEigenvector = start;
+            for (int i = 0; i < total; i++)
+            {
+                double eigenvector1 = realMatrix2.CalculateEigenvector(eigenvalues[0], currentEigenvector);
+                double eigenvector2 = realMatrix2.CalculateEigenvector(eigenvalues[1], currentEigenvector);
+
+                eigenvectors.Add(new double[] { eigenvector1, eigenvector2 });
+                currentEigenvector += increment;
+            }
+
+            return this.Ok(eigenvectors.ToArray());
         }
     }
 }
